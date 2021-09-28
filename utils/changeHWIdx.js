e30ab36a7ac406f54ee15e77d82fc1af1cb6baac
변지욱 사원 DCX플랫폼팀 (jiwookbyun@lgcns.com <jiwookbyun@CNS.LGCNS.COM>
2021-09-28 14:04:27 +0900

    순서 변경에서 버그가 발생하던 부분 수정

--- utils/changeHWIdx.js
+++ utils/changeHWIdx.js
@@ -2,7 +2,7 @@ const UserLoado = require("../models/UserLoado");
 
 // 순서를 1,2,3 이렇게 sequential 하게 변경해줘야 함... 이러지 않으면 순서변경 기능에 버그가 발생
 const changeHWIdx = async (userId) => {
-  const userHomeworks = await UserLoado.find({ user: userId });
+  const userHomeworks = await UserLoado.find({ user: userId }).sort("idx");
   userHomeworks.map(async (item, idx) => {
     item.idx = idx + 1;
     await item.save();
