8a9c688779d31219d93a61efc9acaa69badc7efc
biglol <biglol@nate.com>
2021-10-19 23:52:32 +0900

    doing dashboard

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,9 @@
-const User = require("../models/Users");
-const UserLoado = require("../models/UserLoado");
-const ErrorResponse = require("../utils/errorResponse");
-const asyncHandler = require("../middleware/async");
-var moment = require("moment");
+const User = require('../models/Users');
+const UserLoado = require('../models/UserLoado');
+const LoadoLogs = require('../models/LoadoLogs');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+var moment = require('moment');
 
 // ************ Admin functionality ************
 
@@ -12,7 +13,7 @@ var moment = require("moment");
 exports.updateLoginDate = asyncHandler(async (req, res, next) => {
   const users = await User.find({ lastLogin: { $eq: null } });
   users.map((user, idx) => {
-    user.lastLogin = "2021-09-14 00:00:00";
+    user.lastLogin = '2021-09-14 00:00:00';
     user.save();
   });
   res.status(200).json({
@@ -35,5 +36,55 @@ exports.changeAllPeopleNotification = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
+  });
+});
+
+Date.prototype.addHours = function (h) {
+  this.setTime(this.getTime() + h * 60 * 60 * 1000);
+  return this;
+};
+
+// @desc        Get User log counts
+// @route       GET /api/v1/auth/getDailyLogCounts
+// @access      Private
+exports.getDailyLogCounts = asyncHandler(async (req, res, next) => {
+  const logArray = [];
+  for (let index = 0; index < 7; index++) {
+    const startDate =
+      moment()
+        .add((index + 1) * -1, 'days')
+        .format('YYYY-MM-DD') + ' 15:00:00';
+    const endDate =
+      moment()
+        .add(index * -1, 'days')
+        .format('YYYY-MM-DD') + ' 15:00:00';
+    const dateValue = moment()
+      .add((index + 1) * -1, 'days')
+      .add(9, 'hours')
+      .format('YYYY-MM-DD');
+
+    const logsCount = await LoadoLogs.find()
+      .where('createdDttm')
+      .gt(startDate)
+      .lt(endDate)
+      .select('-stringParam')
+      .countDocuments();
+
+    logArray.push({
+      dateValue,
+      count: logsCount,
+    });
+  }
+
+  const userLoginCount = await User.find()
+    .where('lastLogin')
+    .gt(moment().add(-1, 'days').format('YYYY-MM-DD') + ' 15:00:00')
+    .lt(moment().add(0, 'days').format('YYYY-MM-DD') + ' 15:00:00')
+    .countDocuments();
+
+  res.status(200).json({
+    success: true,
+    log: logArray,
+    userLoginCount,
   });
 });
--- routes/adminFeatures.js
+++ routes/adminFeatures.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
 const {
   updateLoginDate,
   changeAllPeopleNotification,
-} = require("../controllers/adminController");
+  getDailyLogCounts,
+} = require('../controllers/adminController');
 
 const router = express.Router();
 
-const { protectAdmin } = require("../middleware/auth");
+const { protectAdmin } = require('../middleware/auth');
 
-router.route("/updateLoginDate").post(protectAdmin, updateLoginDate);
+router.route('/updateLoginDate').post(protectAdmin, updateLoginDate);
 router
-  .route("/changeAllPeopleNotification")
+  .route('/changeAllPeopleNotification')
   .put(protectAdmin, changeAllPeopleNotification);
+router.route('/adminData/dailyLogsData').get(protectAdmin, getDailyLogCounts);
 
 module.exports = router;
