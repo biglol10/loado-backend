42cb0f2bb21f543cb3a66c84bab2daeb960fb187
변지욱 사원 DCX플랫폼팀 (jiwookbyun@lgcns.com <biglol@nate.com>
2021-12-16 15:10:25 +0900

    added itemprice log feature

--- controllers/itemPriceController.js
+++ controllers/itemPriceController.js
@@ -1,4 +1,5 @@
 const ItemPriceData = require('../models/ItemPrice');
+const LoadoLogs = require("../models/LoadoLogs");
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
@@ -6,31 +7,24 @@ const asyncHandler = require('../middleware/async');
 // @route       GET /loado/api/itemPrice/setItemPrice
 // @access      Public
 exports.setItemPrice = asyncHandler(async (req, res, next) => {
-  const asdf = req.body;
-  console.log(asdf);
+  const dateValue = req.body.dateValue;
+  const itemParam = req.body.jsonParam;
+
+  itemParam.map(item => {
+    ItemPriceData.create({
+      createdDttm: dateValue,
+      itemName: item.itemName,
+      itemPrice: item.itemPrice
+    })
+  });
+
+  await LoadoLogs.create({
+    user: "biglol",
+    activity: "itemPriceLogAdded",
+    stringParam: "",
+  });
 
   return res.status(200).json({
     success: true,
-  });
-
-  const itemName = req.query.itemName;
-  const itemPrice = req.query.itemPrice;
-  const createdDttm = req.query.dateValue;
-
-  // https://stackoverflow.com/questions/9145667/how-to-post-json-to-a-server-using-c
-
-  await ItemPriceData.create({
-    createdDttm,
-    itemName,
-    itemPrice,
-  });
-
-  const userHomeworks = await UserLoado.find().sort('idx');
-  //   const result = userHomeworks.skip(skip).limit(limit);
-
-  res.status(200).json({
-    success: true,
-    dataLength: userHomeworks.length,
-    data: userHomeworks,
   });
 });
