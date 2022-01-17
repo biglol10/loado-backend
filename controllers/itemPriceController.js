ceee5406b679869027f986f6827c6d4b87fb8072
변지욱 사원 DCX기술팀 (jiwookbyun@lgcns.com <biglol@nate.com>
2022-01-17 18:28:01 +0900

    save user item

--- controllers/itemPriceController.js
+++ controllers/itemPriceController.js
@@ -1,4 +1,5 @@
 const ItemPriceData = require("../models/ItemPrice");
+const UserItemTrend = require("../models/UserItemTrend");
 const LoadoLogs = require("../models/LoadoLogs");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
@@ -29,5 +30,60 @@ exports.setItemPrice = asyncHandler(async (req, res, next) => {
 });
 
 // @desc        Save User's interested items
-// @route       POST /loado/api/itemPrice/user
-// @access      Public
+// @route       POST /loado/api/itemPrice/setUserItemInterest
+// @access      Private
+exports.setUserItemInterest = asyncHandler(async (req, res, next) => {
+  const findUserInterest = await UserItemTrend.find({ user: req.user._id });
+  let stringLog = "";
+
+  if (findUserInterest.length !== 0) {
+    const output = await UserItemTrend.findByIdAndUpdate(
+      findUserInterest[0]._id,
+      {
+        interestedItemsArray: req.body.itemCollection,
+      }
+    );
+    stringLog = JSON.stringify({
+      user: output._id,
+      interestedItems: output.interestedItemsArray,
+    });
+  } else {
+    const output = await UserItemTrend.create({
+      user: req.user._id,
+      interestedItemsArray: req.body.itemCollection,
+    });
+    stringLog = JSON.stringify({
+      user: output._id,
+      interestedItems: output.interestedItemsArray,
+    });
+  }
+
+  LoadoLogs.create({
+    activity: "setItemInterest",
+    stringParam: stringLog,
+  });
+
+  return res.status(200).json({
+    success: true,
+  });
+});
+
+// @desc        Get User's interested items
+// @route       GET /loado/api/itemPrice/getUserItemInterest
+// @access      Private
+exports.getUserItemInterest = asyncHandler(async (req, res, next) => {
+  const findUserInterest = await UserItemTrend.find({ user: req.user._id });
+
+  LoadoLogs.create({
+    activity: "getItemInterest",
+    stringParam: JSON.stringify(findUserInterest),
+  });
+
+  return res.status(200).json({
+    success: true,
+    userInterest:
+      findUserInterest.length === 0
+        ? []
+        : findUserInterest[0].interestedItemsArray,
+  });
+});
--- routes/itemPrice.js
+++ routes/itemPrice.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const { setItemPrice } = require('../controllers/itemPriceController');
+const {
+  setItemPrice,
+  setUserItemInterest,
+  getUserItemInterest,
+} = require("../controllers/itemPriceController");
 
 const router = express.Router();
 
-const { protect } = require('../middleware/auth');
+const { protect } = require("../middleware/auth");
 
-router.route('/setItemPrice').post(setItemPrice);
+router.route("/setItemPrice").post(setItemPrice);
+router
+  .route("/userItemInterest")
+  .get(protect, getUserItemInterest)
+  .post(protect, setUserItemInterest);
 
 module.exports = router;
